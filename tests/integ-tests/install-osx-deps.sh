1f44885c93dc703631e4f76bf813d05469383015
Filippo Squillace <squillace.filippo@gmail.com>
2017-08-31 14:05:30 +0200

    Fix integ test

--- tests/integ-tests/install-osx-deps.sh
+++ tests/integ-tests/install-osx-deps.sh
@@ -4,7 +4,8 @@ brew update
 # Coreutils and git should be already installed on OSX 7.3+ images:
 #brew install coreutils git
 # Openssl is required for installing git from source code
-brew install openssl
+# Findutils is required for `fonts` Pearl package
+brew install openssl findutils
 
 ./tests/test-utils/install-bash.sh "$TRAVIS_BASH_VERSION"
 ./tests/test-utils/install-zsh.sh "$TRAVIS_ZSH_VERSION"
