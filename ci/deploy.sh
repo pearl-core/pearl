cc30f991bf2e37610eb0ee900c7790f24952cd27
Filippo Squillace <squillace.filippo@gmail.com>
2020-01-16 23:35:40 +0100

    Replace creds twine with token

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,7 +2,7 @@
 
 set -e
 export TWINE_REPOSITORY="https://test.pypi.org/legacy/"
-export TWINE_USER="fsquillace"
+export TWINE_USER="__token__"
 
 [[ "$TRAVIS_OS_NAME" == "osx" ]] && [[ "$PYTHON_VERSION" == "3.5" ]] && [[ "$TRAVIS_BRANCH" == "master" ]] && make release-ci
 
